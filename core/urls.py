0fe37978f735659e97af9fe9aad10177ee7a20e8
Ignacio Martinez <i.martinez@duocuc.cl>
2021-06-21 22:52:13 -0400

    añadi muchas cosas xddd

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, libros, registro
+from .views import home, libros, registro, form_libro, form_mod_libro
 
 urlpatterns = [
     path('',home,name ="home"),
     path('libros',libros,name ="libros"),
     path('registro',registro,name ="registro"),
+    path('form-libro',form_libro,name ="form_libro"),
+    path('modificar-libro/<id>',form_mod_libro,name ="form_mod_libro"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Libro
+from .forms import LibroForm
 
 # Create your views here.
 
@@ -18,3 +19,28 @@ def libros(request):
     }
 
     return render(request, 'core/libros.html', datos)
+
+def form_libro(request):
+    datos = {
+        'form': LibroForm()
+    }
+    if request.method == 'POST':
+        formulario = LibroForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Libro ingresado"
+    
+
+    return render(request, 'core/form_libro.html', datos)
+
+def form_mod_libro(request,id):
+    libro = Libro.objects.get(ISBN=id)
+    datos={
+        'form': LibroForm(instance=libro)
+    }
+    if request.method == 'POST':
+    formulario = LibroForm(data=request.POST, instance=libro)
+        if formulario.is_valid:
+        formulario.save()
+        datos['mensaje'] = "Datos modificados"
+    return render(request, 'core/form_mod_libro.html', datos)
